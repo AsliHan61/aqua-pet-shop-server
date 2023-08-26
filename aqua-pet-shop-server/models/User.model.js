e2ab1effff489fb25d9f439606feaa32fc0aef91
AsliHan61 <asli.61.y@gmail.com>
2023-08-26 13:41:28 +0200

    some Style

--- aqua-pet-shop-server/models/User.model.js
+++ aqua-pet-shop-server/models/User.model.js
@@ -3,6 +3,19 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    firstName: {
+      type: String,
+    },
+
+    lastName: {
+      type: String,
+    },
+
+    userName:{
+      type: String,
+      required: [true, 'Username is required.']
+    },
+
     email: {
       type: String,
       required: [true, 'Email is required.'],
@@ -10,13 +23,37 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true
     },
+
     password: {
       type: String,
       required: [true, 'Password is required.']
+    },
+
+    address: {
+      type: String,
+    },
+
+    zipCode: {
+      type: String,
+    },
+    
+    city: {
+      type: String,
+    },
+    
+    country: {
+      type: String,
+    },
+
+    imageUrl: {
+      type: String,
+    } ,
+
+    ordersId: {
+      type: String,
     }
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
     timestamps: true
   }
 );
