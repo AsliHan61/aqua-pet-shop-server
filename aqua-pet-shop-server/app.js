f170eb1f4ef49f1628f10e69c96a0f460327046e
AsliHan61 <asli.61.y@gmail.com>
2023-09-06 17:08:15 +0200

    SomeChanges

--- aqua-pet-shop-server/app.js
+++ aqua-pet-shop-server/app.js
@@ -3,7 +3,7 @@
 require("dotenv").config();
 
 // ℹ️ Connects to the database
-require("./aqua-pet-shop-server/db");
+require("./db");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
@@ -12,25 +12,25 @@ const express = require("express");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./aqua-pet-shop-server/config")(app);
+require("./config")(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./aqua-pet-shop-server/routes/index.routes");
+const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
-const productsRoutes = require("./aqua-pet-shop-server/routes/products.routes");
+const productsRoutes = require("./routes/products.routes");
 app.use("/api", productsRoutes);
 
-const extraServiceRoutes = require("./aqua-pet-shop-server/routes/extraservice.routes");
+const extraServiceRoutes = require("./routes/extraservice.routes");
 app.use("/api", extraServiceRoutes);
 
-const orderRoutes = require("./aqua-pet-shop-server/routes/order.routes");
+const orderRoutes = require("./routes/order.routes");
 app.use("/api", orderRoutes);
 
-const authRoutes = require("./aqua-pet-shop-server/routes/auth.routes");
+const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./aqua-pet-shop-server/error-handling")(app);
+require("./error-handling")(app);
 
 module.exports = app;
--- aqua-pet-shop-server/routes/index.routes.js
+++ aqua-pet-shop-server/routes/index.routes.js
@@ -1,7 +1,39 @@
 const router = require("express").Router();
+const express = express.Router();
+
+const User = require("../models/User.model");
+const { isAuthenticated } = require("../middlewares/jwt.middleware");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.post("/upload", fileUploader.single("image"), (req, res) =>{
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  console.log(req.file)
+
+  res.json({ image: req.file.path });
+})
+
+router.get("/users", isAuthenticated, (req, res) => {
+  console.log('payload', req.payload)
+  res.status(200).json(req.payload)
+
+})
+
+
+router.put("/users", (req, res) =>{
+  const {_id, image } =req.body;
+
+  User.findbyIdAndUpdate(_id, {image}, {new: true})
+  .then(updatedUser => {
+    const {_id, username, image } = updatedUser
+    res.json({ updatedUser: { _id, username, image } })
+  })
+  .catch(err => console.error(err))
+})
 
 module.exports = router;
